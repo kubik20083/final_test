5325774f420540eab01c3ecf931b03e04a3f0dcd
kubik20083 <kubik20083@mail.ru>
2024-02-25 17:10:46 +0300

    animal registry

---
+++ AnimalRegistry/App.java
@@ -0,0 +1,7 @@
+package AnimalRegistry;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ AnimalRegistry/Pets.java
@@ -0,0 +1,29 @@
+package AnimalRegistry;
+
+public abstract class Pets {
+    protected int id;
+    protected String name;
+
+    public Pets(int id, String name) {
+        this.id = id;
+        this.name = name;
+
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
